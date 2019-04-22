0d69a4002d1b08ab37d23d157cf55927165e830d
unknown <vania.paramita23@gmail.com>
2019-04-22 13:44:00 +0700

    New Calculator

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 //import logo from './logo.svg';
 import './App.css';
 import Profile from './Profile.js';
+import Calculator from './Calculator.js';
 
 class App extends Component {
   constructor(props){
@@ -13,54 +14,7 @@ class App extends Component {
       color : 'grey',
       img : 'Home.jpg',
       img2 : 'home.png',
-      nilai1 : null,
-      nilai2: null,
-      operator: null,
-      hasil: null,
     }
-
-  }
-  handleSubmit(e){
-	  e.preventDefault();
-	  
-	  const nilai1 = parseInt(this.refs.nilai1.value);
-	  const nilai2 = parseInt(this.refs.nilai2.value);
-	  const operator = this.refs.operator.value;
-	  
-	  var hasil;
-	  switch(operator){
-		  case '+': 
-			hasil = nilai1 + nilai2;
-			break;
-		  case '-':
-			hasil = nilai1 - nilai2;
-			break;
-		  case '*':
-			hasil = nilai1 * nilai2;
-			break;
-		  case '/':
-		  hasil = nilai1 / nilai2;
-			break;
-	  }
-	  this.setState({nilai1, nilai2, operator, hasil});
-	  
-	  this.refs.nilai1.value = null;
-	  this.refs.nilai2.value = null;
-  }
-  
-  renderHasil(){
-	  const {nilai1, nilai2, operator, hasil} = this.state;
-	  if(this.state.hasil){
-		  return (
-			<div>
-				<br />
-				<p className="alert alert-success">
-				{"Hasilnya adalah "+hasil}
-				</p>
-			</div>	
-		  );
-	  }
-	  
   }
 
   clicked(menu) {
@@ -75,30 +29,8 @@ class App extends Component {
 
   info(name){
     if(name === "Calculator"){
-      return(
-        <div>
-          <span className={"selected "+this.state.color}>{this.state.name}</span>
-            <br></br>
-            <br></br>
-          <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="number" ref="nilai1"></input>
-  			  <select ref="operator" className="form-control">
-				  <option value="+">+</option>
-				  <option value="-">-</option>
-				  <option value="*">*</option>
-				  <option value="/">/"</option>
-			    </select>
-          <input type="number" ref="nilai2"></input> =...
-          <br></br>
-          <br></br>
-          <button className="btn btn-primary">Count Me</button>
-          </form>
-          {this.renderHasil()}
-        </div>
-      )
-      
+      return <Calculator/>
     }
-
     else if(name === 'Profile'){
       return <Profile/>;
     }
@@ -116,22 +48,7 @@ class App extends Component {
     </div>);
     }
   }
-  /*menusaya(){
-    if (this.state.active.toLowerCase()==='Red'){
-			gambar = "Red";
-		}
-		else if (this.state.active.toLowerCase()==='Yellow'){
-			gambar = "Yellow";
-		}
-		else if (this.state.active.toLowerCase()==='Green'){
-			gambar = "Green";
-		}
-		else if (this.state.active.toLowerCase()==='Blue'){
-			gambar = "Blue";
-		} 
-    return <img src={this.state.color+".jpg"} width="240"></img>
-}*/
-
+ 
   render(){
     return(
       <div id="app">
@@ -158,4 +75,20 @@ class App extends Component {
   </div>
     ); 
 }}
+ /*menusaya(){
+    if (this.state.active.toLowerCase()==='Red'){
+			gambar = "Red";
+		}
+		else if (this.state.active.toLowerCase()==='Yellow'){
+			gambar = "Yellow";
+		}
+		else if (this.state.active.toLowerCase()==='Green'){
+			gambar = "Green";
+		}
+		else if (this.state.active.toLowerCase()==='Blue'){
+			gambar = "Blue";
+		} 
+    return <img src={this.state.color+".jpg"} width="240"></img>
+}*/
+
 export default App;    
